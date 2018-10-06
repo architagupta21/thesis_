8dc619aaa95bc600584a7c4bd7f6f8aac75ecf99
Ankith Konda <ankith.konda@gmail.com>
2018-10-06 23:38:45 +1000

    Add comments to  interface

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,9 +1,30 @@
 declare module "redux-axios-middleware";
 
 interface $LTI {
+  /**
+   * The LTI ID
+   *
+   * @type {string}
+   * @memberof $LTI
+   */
   id: string;
+  /**
+   * User ID of the learner is currently interacting with the LTI
+   *
+   * Note: This is always set to "student" when the LTI is viwed in edit view
+   * of the edX platform.
+   *
+   * @type {string}
+   * @memberof $LTI
+   */
   userID: string;
-  courseID: string;
+  /**
+   * This can either be Instructor | Administrator | Student
+   *
+   *
+   * @type {string}
+   * @memberof $LTI
+   */
   user_role: string;
 }
 
